fe98f9e68a1f9c7ddaa460d1d2d5b686de2ed635
vanessa.jalakas <vanessa.jalakas@gmail.com>
2019-02-26 20:03:12 +0200

    initial commit

--- tthk.ContactsRegistry/Startup.cs
+++ tthk.ContactsRegistry/Startup.cs
@@ -1,12 +1,18 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using tthk.ContactsRegistry.Data;
 
-namespace tthk.ContactsRegistry
+namespace WebApplication1
 {
     public class Startup
     {
@@ -22,12 +28,36 @@ namespace tthk.ContactsRegistry
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            //services.AddScoped<IListItemsService, ListItemsService>();
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddDbContext<TestContext>(options =>
+                    //options.UseInMemoryDatabase("test")
+                    options.UseSqlServer(Configuration.GetConnectionString("foo"))
+                    );
+
+            //services.AddMiniProfiler();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("hard-coded-secret")),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -45,6 +75,10 @@ namespace tthk.ContactsRegistry
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+
+            //app.UseMiniProfiler();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
